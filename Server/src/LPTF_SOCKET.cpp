5a03a8a6a9dcb9e48a1ecf4545b458571a9b334b
cyril <cyril.porez@laplateforme.io>
2024-04-23 10:20:09 +0200

    feat init  syscall method

--- Server/src/LPTF_SOCKET.cpp
+++ Server/src/LPTF_SOCKET.cpp
@@ -1,8 +1,60 @@
 #include <iostream>
+#include <string.h>
+#include <sys/socket.h>
 
 class LPTF_SOCKET
 {
   private:
+    int sockfd;
+    sockaddr_in addr;
   
   public:
+    LPTF_SOCKET() {
+      sockfd = socket(AF_INET, SOCK_STREAM, 0);
+      if (sockfd < 0)
+      {
+        printf("Echec cde la création du socket");
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    void accept()
+    {
+
+    }
+
+    void bind()
+    {
+
+    }
+
+    void connect()
+    {
+
+    }
+
+    void listen()
+    {
+
+    }
+
+    void select()
+    {
+
+    }
+
+    void read()
+    {
+
+    }
+
+    void recv()
+    {
+
+    }
+
+    void send()
+    {
+      
+    }
 }
