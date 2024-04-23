5a493c50308d3ddf46d970d12d507518c36f3f20
thibault-kine <91733294+thibault-kine@users.noreply.github.com>
2024-04-23 14:39:11 +0200

    listen

--- Server/src/LPTF_SOCKET.cpp
+++ Server/src/LPTF_SOCKET.cpp
@@ -2,64 +2,67 @@
 #include <string.h>
 #include <winsock2.h>
 
+#pragma comment(lib, "Ws2_32.lib")
+
 class LPTF_SOCKET
 {
-  private:
+private:
     int sockfd;
     sockaddr_in addr;
-  
-  public:
+
+public:
     LPTF_SOCKET() {
-      sockfd = socket(AF_INET, SOCK_STREAM, 0);
-      if (sockfd == INVALID_SOCKET)
-      {
-        std::cerr << "Fail to create socket: " << WSAGetLastError()<< std::endl;
-      }
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if (sockfd == INVALID_SOCKET) {
+            std::cerr << "Fail to create socket: " << WSAGetLastError() << std::endl;
+        }
     }
 
     void accept()
     {
-
     }
 
     void bind()
     {
-
     }
 
-    void connect()
-    {
-      connect();
+    void connect() {
+        connect();
     }
 
     /**
      * @param socket Un socket lié par la méthode ```bound()``` et non-connecté
      * @param backlog La taille maximum de la queue pour les connexions en attente
-    */
-    void listenLPTFSocket(SOCKET socket, int backlog) {
-        if(listen(socket, backlog) == SOCKET_ERROR) 
+     */
+    int listenLPTFSocket() {
+        // On essaye d'écouter avec le socket de la classe et la taille 
+        // de file d'attente maximum
+        if (listen(this->sockfd, SOMAXCONN) == SOCKET_ERROR)
             printf("listenLPTFSocket() function failed with error %d\n", WSAGetLastError());
-        else
+        else {
             printf("Listening of socket...");
+            return 0;
+        }
     }
 
     void select()
     {
-
     }
 
     void read()
     {
-
     }
 
     void recv()
     {
-
     }
 
     void send()
     {
-      
+    }
+
+    SOCKET getSocket()
+    {
+        return this->sockfd;
     }
 };
