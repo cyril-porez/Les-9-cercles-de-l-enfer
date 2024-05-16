19efed254202701041e85d245fee004d87b4ad9f
cyril <cyril.porez@laplateforme.io>
2024-05-16 17:20:33 +0200

    delete commentary

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -6,92 +6,23 @@
 
 int main()
 {
-  LPTF_Socket serverSocket("127.0.0.1", 8080, true);
-  std::cout << "Server initialized\n";
+    LPTF_Socket serverSocket("127.0.0.1", 8080, true);
+    std::cout << "Server initialized\n";
 
-  if (serverSocket.bind() != 0)
-  {
-    std::cerr << "Error: Server cannot bind\n";
-    return 1;
-  }
+    if (serverSocket.bind() != 0)
+    {
+        std::cerr << "Error: Server cannot bind\n";
+        return 1;
+    }
 
-  if (serverSocket.listen() != 0)
-  {
-    std::cerr << "Error: Server cannot listen\n";
-    return 1;
-  }
+    if (serverSocket.listen() != 0)
+    {
+        std::cerr << "Error: Server cannot listen\n";
+        return 1;
+    }
 
-  std::cout << "Server ready to go!\n";
-  serverSocket.handleMultipleClients();
+    std::cout << "Server ready to go!\n";
+    serverSocket.handleMultipleClients();
 
-  return 0;
+    return 0;
 }
-
-//  std::list<SOCKET> clientSockets;
-//   fd_set readfs;
-//   SOCKET maxSocket = serverSocket.getSocket();
-
-//   std::cout << "Server is listening for incoming connections...." << std::endl;
-
-//   while (true)
-//   {
-//     FD_ZERO(&readfs);
-//     FD_SET(serverSocket.getSocket(), &readfs);
-
-//     for (SOCKET sock : clientSockets)
-//     {
-//       FD_SET(sock, &readfs);
-//     }
-
-//     int activity = select(maxSocket + 1, &readfs, NULL, NULL, NULL);
-//     if ((activity < 0) && (WSAGetLastError() != WSAEINTR))
-//     {
-//       std::cerr << "Select error.\n";
-//       continue;
-//     }
-
-//     if (FD_ISSET(serverSocket.getSocket(), &readfs))
-//     {
-//       SOCKET newSocket = serverSocket.acceptLPTFSocket();
-//       if (newSocket != INVALID_SOCKET)
-//       {
-//         clientSockets.push_back(newSocket);
-//         std::cout << "New connection accepted.\n";
-//       }
-//     }
-
-//     for (auto it = clientSockets.begin(); it != clientSockets.end();)
-//     {
-//       SOCKET sock = *it;
-//       if (FD_ISSET(sock, &readfs))
-//       {
-//         char buffer[1024];
-//         int bytesRead = recv(sock, buffer, 1024, 0);
-//         if (bytesRead == 0)
-//         { // Client disconnected
-//           closesocket(sock);
-//           it = clientSockets.erase(it);
-//           std::cout << "Client disconnected.\n";
-//         }
-//         else if (bytesRead > 0)
-//         {
-//           buffer[bytesRead] = '\0';
-//           std::cout << "Received: " << buffer << std::endl;
-//           send(sock, buffer, bytesRead, 0);
-//           ++it;
-//         }
-//         else
-//         {
-//           ++it;
-//         }
-//       }
-//       else
-//       {
-//         ++it;
-//       }
-//     }
-
-//     WSACleanup();
-
-//     return 0;
-//   }
