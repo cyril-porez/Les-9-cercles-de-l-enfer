5c070288b8a939a65c38a8893973075e581be3dc
cyril-porez <91790422+cyril-porez@users.noreply.github.com>
2024-06-28 18:04:19 +0200

    Delete GUI/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug directory

--- GUI/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_GUI_FILE C:/Users/porez/OneDrive/Bureau/GUI/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/GUI.exe)
-set(__QT_DEPLOY_TARGET_GUI_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_GUI_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
