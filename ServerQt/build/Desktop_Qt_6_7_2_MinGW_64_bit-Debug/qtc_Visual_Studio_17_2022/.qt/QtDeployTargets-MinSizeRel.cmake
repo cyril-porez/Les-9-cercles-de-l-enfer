93a73139236a283433a154eb1cbae2cbab05b891
cyril-porez <91790422+cyril-porez@users.noreply.github.com>
2024-06-28 17:44:38 +0200

    Delete ServerQt directory

--- ServerQt/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/qtc_Visual_Studio_17_2022/.qt/QtDeployTargets-MinSizeRel.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ServerQt_FILE C:/Users/chezt/OneDrive/Documents/CPP_Projects/Les-9-cercles-de-l-enfer/ServerQt/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/qtc_Visual_Studio_17_2022/MinSizeRel/ServerQt.exe)
-set(__QT_DEPLOY_TARGET_ServerQt_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ServerQt_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
